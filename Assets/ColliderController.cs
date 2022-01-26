62c130b1a7eb4467a6a1741b8e6d86fcf653a22d
29-Jude <jude77787@gmail.com>
2022-01-26 10:09:28 +0800

    Added a CollisionCollider Script
    - Added a script to the player to control collision with the water and the coins

---
+++ Assets/ColliderController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ColliderController : MonoBehaviour
+{
+    public float CoinsToWin;
+    public float CoinsCollected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CoinsCollected == CoinsToWin)
+        {
+            SceneManager.LoadScene("GameWin");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Coin")
+        {
+            CoinsCollected += 1;
+            Destroy(collision.gameObject);
+        }
+
+        else if(collision.gameObject.tag == "Fire")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
