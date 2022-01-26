9cf4ad9e6d84f97cde1085a9661f8415073af250
29-Jude <jude77787@gmail.com>
2022-01-26 10:04:09 +0800

    Added GameWin and GameLose Scenes
    - Created 2 more scenes for the winning and losing

---
+++ Assets/RestartController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartController : MonoBehaviour
+{
+    public string SceneToLoad;
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
