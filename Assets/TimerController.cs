2e5eed6d57c0975a4c9ee3dc6ccdd4da873aa516
29-Jude <jude77787@gmail.com>
2022-01-27 10:24:17 +0800

    Added a countdown timer
    - Added a timer so that the player won't be able to chill and do nothing the whole time lol

---
+++ Assets/TimerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimerController : MonoBehaviour
+{
+    public float timeRemaining;
+    public bool timerIsRunning = false;
+    public Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerIsRunning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerIsRunning == true)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+
+            else if (timeRemaining <= 0)
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                SceneManager.LoadScene("GameLose");
+            }
+        }
+
+        DisplayTime(timeRemaining);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
